4e17abed6faa630d17cce832e69738b37f99d724
WmsoftInformatica <welton.wmsoft@gmail.com>
2019-04-12 11:59:58 -0300

    adicionada classe Auxiliar

---
+++ ola mundo/src/com/wmsoft/Auxiliar.java
@@ -0,0 +1,8 @@
+package com.wmsoft;
+
+public class Auxiliar {
+
+	public void exibirMenssagem(String pMenssagem) {
+		System.out.println(pMenssagem);
+	}
+}
--- ola mundo/src/com/wmsoft/Principal.java
+++ ola mundo/src/com/wmsoft/Principal.java
@@ -2,9 +2,16 @@ package com.wmsoft;
 
 public class Principal {
 
+	static String olaMundo = "ola mundo!!!";
+	static String olaMarte = "ola marte!!!";
+	
+		
 	public static void main(String[] args) {
 	
-		System.out.println("ola mundo!!!");
+		Auxiliar auxiliar = new Auxiliar();
+
+		auxiliar.exibirMenssagem(olaMundo);
+		auxiliar.exibirMenssagem(olaMarte);
 
 	}
 
